cbefb9b9a2f05f0c61790b4546df9b88a4b31ba5
changamanda <amanda.chang10@gmail.com>
2015-04-29 12:03:53 -0400

    Initial commit

---
+++ flatiron-scheduler.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name        = 'flatiron-scheduler'
+  s.version     = '0.0.1'
+  s.date        = '2015-04-29'
+  s.summary     = "Flatiron Scheduler!"
+  s.description = "A simple FIS Scheduling Gem"
+  s.authors     = ["Amanda Chang"]
+  s.email       = 'chang@flatironschool.com'
+  s.executables <<  'flatiron-scheduler'
+  s.files       = ["lib/flatiron-scheduler.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/flatiron-scheduler'
+  s.license       = 'MIT'
+end
---
+++ lib/flatiron-scheduler.rb
@@ -0,0 +1,60 @@
+class FlatironScheduler
+  attr_accessor :week, :day
+
+  def initialize(week, day)
+    @path = '/Users/amandachang/Development/staff-ruby007/curriculum/web-0415'
+    @week = week
+    @day = day
+  end
+
+  def file_name
+    "week-#{week}/day-#{day}.md"
+  end
+
+  def move_to_dir
+    Dir.chdir(@path)
+  end
+
+  def checkout_future
+    system('git checkout future')
+  end
+
+  def checkout_master
+    system('git checkout master')
+  end
+
+  def pull
+    system('git pull')
+  end
+
+  def push
+    system('git push')
+  end
+
+  def add_commit
+    system('git add .')
+    system("git commit -am \"Week #{self.week}, Day #{self.day}\"")
+  end
+
+  def checkout_today
+    system("git checkout future -- #{file_name}")
+  end
+
+  def change_symlink
+    system("ln -sfn #{file_name} README.md")
+  end
+
+  def run
+    move_to_dir
+    checkout_future
+    pull
+    add_commit
+    # push
+    checkout_master
+    pull
+    checkout_today
+    change_symlink
+    add_commit
+    # push
+  end
+end
